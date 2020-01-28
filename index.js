bc72b68f00504fbd32967bf79cb02b81833d0fbc
Lili Manrique <liliana.manrique1@gmail.com>
2020-01-28 12:50:07 -0700

    Add comments

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const http = require('http');
 let app = http.createServer();
 const Game = require('../flashcards/src/Game');
+const Round = require('../flashcards/src/Round');
+const Deck = require('../flashcards/src/Deck');
 // Start the server on port 3000
 app.listen(3000, '127.0.0.1');
 console.log('Node server running on port 3000');
 const game = new Game();
+// async function runGame() {
+//   await game.start();
+// }
+// runGame();
 game.start();
+// game.newRound();
