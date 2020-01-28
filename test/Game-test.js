e9c081e9f5a637fbaea1a22f68466a8b584c1272
Lili Manrique <liliana.manrique1@gmail.com>
2020-01-28 16:54:24 -0700

    Uncomment game require

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-// const Game = require('../src/Game');
+const Game = require('../src/Game');
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Round = require('../src/Round');
