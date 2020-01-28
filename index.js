8b1785f89590ece0ab737288a610e94df9a012b3
Lili Manrique <liliana.manrique1@gmail.com>
2020-01-28 16:52:55 -0700

    Shorten require paths

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const http = require('http');
 let app = http.createServer();
-const Game = require('../flashcards/src/Game');
-const Round = require('../flashcards/src/Round');
-const Deck = require('../flashcards/src/Deck');
+const Game = require('./src/Game');
+const Round = require('./src/Round');
+const Deck = require('./src/Deck');
 // Start the server on port 3000
 app.listen(3000, '127.0.0.1');
 console.log('Node server running on port 3000');
