180e82cee7813622ff62dbc011fe6f8e8ae68c5a
Lili Manrique <liliana.manrique1@gmail.com>
2020-01-28 16:15:56 -0700

    Change start function to allow for parameters

--- src/Game.js
+++ src/Game.js
@@ -19,33 +19,18 @@ class Game {
 -----------------------------------------------------------------------`)
   }
 
-  async printQuestion(round) {
-    console.log('1')
-    await util.main(round);
-    console.log('2')
-    this.newRound();
+  printQuestion(round) {
+    util.main(round);
   }
 
-  start() {
-    this.cards = prototypeQuestions.map(card => {
+  start(cards, deck, round) {
+    this.cards = cards || prototypeQuestions.map(card => {
       card = new Card(card.id, card.question, card.answers, card.correctAnswer);
       return card;
     });
-    this.deck = new Deck(this.cards);
-    this.currentRound = new Round(this.deck);
+    this.deck = deck || new Deck(this.cards);
+    this.currentRound = round || new Round(this.deck);
     this.printMessage(this.deck);
-    this.printQuestion(this.currentRound);
-  }
-
-  newRound() {
-    this.incorrectCards = this.currentRound.incorrectGuesses.map(guess => {
-      const targetQuestion = prototypeQuestions[(guess + 1)];
-      guess = new Card (targetQuestion.id, targetQuestion.question, targetQuestion.answers, targetQuestion.correctAnswer);
-      return guess;
-    });
-    let newDeck = new Deck(this.incorrectCards);
-    this.currentRound = new Round(newDeck);
-    this.printMessage(newDeck);
     this.printQuestion(this.currentRound);
   }
 }
