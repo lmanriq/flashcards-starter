6b64cceac6eab959dda641c1e4205689c37f72e0
Lili Manrique <liliana.manrique1@gmail.com>
2020-01-30 13:39:25 -0700

    Write second data set

---
+++ src/data2.js
@@ -0,0 +1,41 @@
+const prototypeData2 = [{
+  "id": 1,
+  "question": "What does HTML stand for?",
+  "answers": ["Hypertext Markup Linker", "Hypertext Markup Language", "Hyperdrive Makeup Language"],
+  "correctAnswer": "Hypertext Markup Language"
+}, {
+  "id": 2,
+  "question": "What does JSON stand for?",
+  "answers": ["JavaScript Object Node", "JavaScript Ordered Nodelist", "JavaScript Object Notation"],
+  "correctAnswer": "JavaScript Object Notation"
+}, {
+  "id": 3,
+  "question": "What does CSS stand for?",
+  "answers": ["Cascading Style Sheets", "Concursive Style Steps", "Cascading Sheet Syntax"],
+  "correctAnswer": "Cascading Style Sheets"
+}, {
+  "id": 4,
+  "question": "Which of the following isn't a valid JavaScript data type?",
+  "answers": ["array", "string", "multiplier"],
+  "correctAnswer": "multiplier"
+}, {
+  "id": 5,
+  "question": "What does DOM stand for?",
+  "answers": ["Data Orientation Method", "Document Object Model", "Data Object Mutation"],
+  "correctAnswer": "Document Object Model"
+}, {
+  "id": 6,
+  "question": "What does DRY stand for?",
+  "answers": ["Don't Repeat Yourself", "Develop Readable Yarn", "Despise Repeating Yourself"],
+  "correctAnswer": "Don't Repeat Yourself"
+}, {
+  "id": 7,
+  "question": "What are passed into functions with parameters when they're called?",
+  "answers": ["paramters", "arguments", "objects"],
+  "correctAnswer": "arguments"
+}, {
+  "id": 8,
+  "question": "What does TDD stand for?",
+  "answers": ["Tutoring Devised Development", "Torturing Dumb Dorks", "Test Driven Development"],
+  "correctAnswer": "callback function"
+}
